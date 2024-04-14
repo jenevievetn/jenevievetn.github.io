6d4b7bdae4d9a2eb4d54d69996f62ff328aee21c
jenevievetn <163372741+jenevievetn@users.noreply.github.com>
2024-04-15 01:40:59 +0800

    app3

--- app3.R/app.R
+++ app3.R/app.R
@@ -36,9 +36,9 @@ server <- function(input, output) {
     emotion_data <- top10_emotion_avg_long[top10_emotion_avg_long$Emotion == input$emotion, ]
     
     # Plot the lollipop graph
-    p <- ggplot(emotion_data, aes(x = reorder(`Character Name`, `Average Sentiment`), y = `Average Sentiment`)) +
-      geom_segment(aes(xend = `Character Name`, yend = 0, color = `Character Name`), size = 1) +
-      geom_point(aes(color = `Character Name`), size = 4, alpha = 0.6) +
+    p <- ggplot(emotion_data, aes(x = reorder(`Character.Name`, `Average.Sentiment`), y = `Average.Sentiment`)) +
+      geom_segment(aes(xend = `Character.Name`, yend = 0, color = `Character.Name`), linewidth = 1) +
+      geom_point(aes(color = `Character.Name`), size = 4, alpha = 0.6) +
       labs(title = paste("Average", input$emotion, "Sentiments by Character"),
            x = "Characters",
            y = "Average Sentiment") +
@@ -63,15 +63,3 @@ server <- function(input, output) {
 
 # Run the application
 shinyApp(ui = ui, server = server)
-
-# STEP 1 – INSTALL RSCONNECT
-#install.packages('rsconnect')
-
-# STEP 2 – AUTHORIZE ACCOUNT
-#rsconnect::setAccountInfo(name='jenevievetn',
-#                          token='1F55D0BF747C0A8BFB032D840C3C72E8',
-#                          secret='ALSFWGD0EaoNnwMbhpqPzVqgfa+VBGBLhY/h6Dmr')
-
-# STEP 3 – DEPLOY
-#library(rsconnect)
-#rsconnect::deployApp('C:/Users/Jen.ACER-JN/Documents/SKOOO/Y1 S2 Modules 2024/NM2207/Project/jenevievetn.github.io/app3.R')
