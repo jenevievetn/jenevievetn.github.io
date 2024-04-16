9d5827c633fd50033e632ad97d3e42c03eef2732
jenevievetn <163372741+jenevievetn@users.noreply.github.com>
2024-04-16 08:03:03 +0800

    personal app

--- personal app/app.R
+++ personal app/app.R
@@ -1,12 +1,18 @@
 library(ggplot2)
 library(shiny)
 library(tidyverse)
+library(plotly)
 
 # Load the data
 avg_emotion_per_chapter <- read.csv("avg_emotion_per_chapter.csv")
+text_by_chapter <- read.csv("text_by_chapter.csv")
 
-# Convert book_title to factor with desired order
-avg_emotion_per_chapter$book_title <- factor(avg_emotion_per_chapter$book_title, levels = unique(avg_emotion_per_chapter$book_title))
+filtered_data <- text_by_chapter[!is.na(text_by_chapter$chapter), ]
+
+# Custom column headers in avg_emotion_per_chapter
+# Rename the columns
+colnames(avg_emotion_per_chapter) <- 
+  c("Book", "Chapter", "Anger", "Anticipation", "Disgust", "Fear", "Joy", "Sadness", "Surprise", "Trust")
 
 # Define custom emotion names
 custom_emotion_names <- c("Anger", "Anticipation", "Disgust", "Fear", "Joy", "Sadness", "Surprise", "Trust")
@@ -21,27 +27,49 @@ ui <- fluidPage(
   titlePanel(
     div(
       style = "font-size: 23px;",  # Adjust font size here
-      "Individual Emotional Sentiment of the Harry Potter Series"
+      "Emotional Trends Across the Harry Potter Series"
     )
   ),
   
-  # Sidebar layout with input and output definitions
-  sidebarLayout(
+  # Add tabset panel for multiple tabs
+  tabsetPanel(
     
-    # Sidebar panel for inputs
-    sidebarPanel(
-      
-      # Dropdown menu for selecting emotion
-      selectInput("emotion", "Select Emotion:",
-                  choices = custom_emotion_names)),
+    # First tab for sentiment analysis
+    tabPanel("Sentiment Analysis",
+             sidebarLayout(
+               sidebarPanel(
+                 # Dropdown menu for selecting emotion
+                 selectInput("emotion", "Select Emotion:",
+                             choices = custom_emotion_names)),
+               mainPanel(
+                 plotlyOutput("sentiment_plot")
+               )
+             )),
     
-    # Main panel for displaying outputs
-    mainPanel(
-      
-      # Plot output for sentiment chart
-      plotOutput("sentiment_plot")
-      
-    )
+    # Second tab for text summaries by chapter
+    tabPanel("Chapter Summary",
+             sidebarLayout(
+               sidebarPanel(
+                 # Text input for chapter number
+                 textInput("chapter_number", "Enter chapter number seen on graph:")),
+               mainPanel(
+                 # Output box for book title
+                 tags$div(
+                   h4("Book Title"),
+                   verbatimTextOutput("book_title_output")
+                 ),
+                 # Output box for book chapter
+                 tags$div(
+                   h4("Book Chapter"),
+                   verbatimTextOutput("chapter_output")
+                 ),
+                 # Output box for summary
+                 tags$div(
+                   h4("Summary"),
+                   verbatimTextOutput("summary_output")
+                 )
+               )
+             ))
   )
 )
 
@@ -49,14 +77,14 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   
   # Render the sentiment plot
-  output$sentiment_plot <- renderPlot({
+  output$sentiment_plot <- renderPlotly({
     
     # Get the selected emotion
     emotion <- input$emotion
     
     # Plot sentiment chart for the selected emotion
-    plot <- ggplot(avg_emotion_per_chapter, aes(x = chapter, y = !!sym(paste0("avg_", tolower(emotion), "_sentiment")), color = book_title)) +
-      geom_line(linewidth = 1) + # Increase the weight of the line
+    plot <- ggplot(avg_emotion_per_chapter, aes(x = Chapter, y = !!sym(emotion), color = Book)) +
+      geom_line(linewidth = 0.5) + # Increase the weight of the line
       labs(title = paste0(emotion, " Sentiment Analysis"),
            x = "Chapter",
            y = "Average Sentiment",
@@ -70,30 +98,88 @@ server <- function(input, output, session) {
                                         colour = "grey95"),
         
         panel.grid.major = element_line(colour = "grey"),  # Make the major grid lines shaded grey
-        legend.position = "right",  # Move legend to the bottom
-        legend.text = element_text(size = 7), # Change the font size of legend text
+        legend.position = "bottom",  # Move legend to the bottom
+        legend.text = element_text(size = 6), # Change the font size of legend text
         legend.title = element_text(size = 7), # Change the font size of legend title
         legend.spacing.y = unit(0.1, "cm")      # Adjust legend item spacing
       ) +
       scale_color_manual(values = custom_colors)  # Set custom hex colors for book titles
     
-    # Print the plot
-    print(plot)
+    # Convert ggplot to plotly object
+    ggplotly(plot)
   })
+  
+  # Render book title
+  output$book_title_output <- renderText({
+  # Get the selected chapter number
+  chapter_number <- input$chapter_number
+  
+  if (!is.na(chapter_number) && chapter_number != "") {
+    # Convert chapter_number to numeric format
+    chapter_number <- as.integer(chapter_number)
+    # Filter the text_by_chapter dataset for the selected chapter
+    filtered_text <- filtered_data[filtered_data$chapter == chapter_number, c("book_title", "chapter_name", "summary")]
+    
+    # Return the corresponding book title
+    if (nrow(filtered_text) > 0) {
+      filtered_text$book_title
+    } else {
+      "Chapter not found."
+    }
+  } else {
+    # If no input is provided, display a default message
+    "Give an input."
+  }
+})
+
+# Render book chapter 
+output$chapter_output <- renderText({
+  input$chapter_number
+  
+  # Get the selected chapter number
+  chapter_number <- input$chapter_number
+  
+  if (!is.na(chapter_number) && chapter_number != "") {
+    # Convert chapter_number to numeric format
+    chapter_number <- as.integer(chapter_number)
+    # Filter the text_by_chapter dataset for the selected chapter
+    filtered_text <- filtered_data[filtered_data$chapter == chapter_number, c("book_title", "chapter_name", "summary")]
+    # Return the corresponding summary
+    if (nrow(filtered_text) > 0) {
+      filtered_text$chapter
+    } else {
+      "Chapter not found."
+    }
+  } else {
+    # If no input is provided, display a default message
+    "Give an input."
+  }
+})
+
+# Render text summary for the selected chapter
+output$summary_output <- renderText({
+  # Get the selected chapter number
+  chapter_number <- input$chapter_number
+  
+  if (!is.na(chapter_number) && chapter_number != "") {
+    # Convert chapter_number to numeric format
+    chapter_number <- as.integer(chapter_number)
+    # Filter the text_by_chapter dataset for the selected chapter
+    filtered_text <- filtered_data[filtered_data$chapter == chapter_number, c("book_title", "chapter_name", "summary")]
+    # Return the corresponding summary
+    if (nrow(filtered_text) > 0) {
+      filtered_text$summary
+    } else {
+      "Chapter not found."
+    }
+  } else {
+    # If no input is provided, display a default message
+    "Give an input."
+  }
+})
+  
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
 
-
-# STEP 1 – INSTALL RSCONNECT
-#install.packages('rsconnect')
-
-# STEP 2 – AUTHORIZE ACCOUNT
-#rsconnect::setAccountInfo(name='jenevievetn',
-#                          token='1F55D0BF747C0A8BFB032D840C3C72E8',
-#                          secret='ALSFWGD0EaoNnwMbhpqPzVqgfa+VBGBLhY/h6Dmr')
-
-# STEP 3 – DEPLOY
-#library(rsconnect)
-#rsconnect::deployApp('C:/Users/Jen.ACER-JN/Documents/SKOOO/Y1 S2 Modules 2024/NM2207/Project/jenevievetn.github.io/personal app')
